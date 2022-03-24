3f9a9fa8b6da203a1d65ade08826ae3db5b202b8
mana <manashams94@gmail.com>
2022-03-24 20:11:10 +0430

    AuthorSerializer & AuthorViewset added.

--- bookworm/library/views.py
+++ bookworm/library/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import AuthorSerializer
+from .models import Author
 
-# Create your views here.
+
+class AuthorViewset(viewsets.ModelViewSet):
+    serializer_class = AuthorSerializer
+    queryset = Author.objects.all()
