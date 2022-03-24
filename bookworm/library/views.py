dadb0ec15ae8d849d3072c688cbb5fb63b70d8c2
mana <manashams94@gmail.com>
2022-03-25 01:23:14 +0430

    Register & LogoutView were set.

--- bookworm/library/views.py
+++ bookworm/library/views.py
@@ -1,5 +1,11 @@
 from rest_framework import viewsets
-from .serializers import AuthorSerializer, BookSerializer, CommentSerializer
+from rest_framework.generics import GenericAPIView
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework import status
+from .serializers import AuthorSerializer, BookSerializer, CommentSerializer, RegisterSerializer, UserSerializer
 from .models import Author, Book, Comment
 
 
@@ -16,3 +22,31 @@ class BookViewset(viewsets.ModelViewSet):
 class CommentViewset(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
     queryset = Comment.objects.all()
+
+
+class Register(GenericAPIView):
+    serializer_class = RegisterSerializer
+
+    def post(self, request, *args,  **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        return Response({
+            "user": UserSerializer(
+                user, context=self.get_serializer_context()
+            ).data, "message": "User Created Successfully",
+        })
+
+
+class LogoutView(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request):
+        try:
+            refresh_token = request.data["refresh_token"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+
+            return Response(status=status.HTTP_205_RESET_CONTENT)
+        except Exception as exc:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
