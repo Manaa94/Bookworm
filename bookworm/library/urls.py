936bce231cde6bf9180fc0be3b7daa55636710d9
mana <manashams94@gmail.com>
2022-03-25 01:24:53 +0430

    urls were set for login, register & logout.

--- bookworm/library/urls.py
+++ bookworm/library/urls.py
@@ -1,5 +1,11 @@
 from rest_framework.routers import DefaultRouter
+from django.urls import path
 from .views import *
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 
 router = DefaultRouter()
 
@@ -8,5 +14,10 @@ router.register(r'books', BookViewset)
 router.register(r'comments', CommentViewset)
 
 
+urlpatterns = router.urls + [
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('register/', Register.as_view()),
+    path('logout/', LogoutView.as_view(), name='auth_logout'),
 
-urlpatterns = router.urls
+]
